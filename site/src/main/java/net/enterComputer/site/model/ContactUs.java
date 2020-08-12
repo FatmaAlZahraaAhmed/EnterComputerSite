f482cef8e10029b30bd645cb23677063eec691a9
FatmaAlZahraaAhmed <48535599+FatmaAlZahraaAhmed@users.noreply.github.com>
2020-08-13 00:28:07 +0200

    Initial commit

---
+++ site/src/main/java/net/enterComputer/site/model/ContactUs.java
@@ -0,0 +1,106 @@
+package net.enterComputer.site.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+
+@Entity(name = "contactus")
+@NamedQueries({
+        @NamedQuery(name = "Contactus.findAll", query = "SELECT c FROM contactus c"),
+        @NamedQuery(name = "Contactus.findByUserName", query = "SELECT c FROM contactus c WHERE c.userName = :userName"),
+        @NamedQuery(name = "Contactus.findByUserEmail", query = "SELECT c FROM contactus c WHERE c.userEmail = :userEmail"),
+        @NamedQuery(name = "Contactus.findBySubject", query = "SELECT c FROM contactus c WHERE c.subject = :subject"),
+        @NamedQuery(name = "Contactus.findByMessage", query = "SELECT c FROM contactus c WHERE c.message = :message")})
+public class ContactUs implements Serializable {
+
+    @Id
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String userName;
+
+    @Email
+    @Size(max = 30)
+    private String userEmail;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String subject;
+
+    @Size(max = 300)
+    private String message;
+
+    public ContactUs() {
+    }
+
+    public ContactUs(String userName) {
+        this.userName = userName;
+    }
+
+    public ContactUs(String userName, String subject) {
+        this.userName = userName;
+        this.subject = subject;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userName != null ? userName.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ContactUs)) {
+            return false;
+        }
+        ContactUs other = (ContactUs) object;
+        if ((this.userName == null && other.userName != null)
+                || (this.userName != null && !this.userName.equals(other.userName))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "net.enterComputer.site ContactForm [ userName=" + userName + " ]";
+    }
+
+}
