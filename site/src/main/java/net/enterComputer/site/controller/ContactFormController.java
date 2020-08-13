b7b611f255c7c39c8f1793c525ec5caebe604e8d
FatmaAlZahraaAhmed <48535599+FatmaAlZahraaAhmed@users.noreply.github.com>
2020-08-13 13:40:52 +0200

    add thymeleaf prefixes

--- site/src/main/java/net/enterComputer/site/controller/ContactFormController.java
+++ site/src/main/java/net/enterComputer/site/controller/ContactFormController.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class ContactFormController {
 
-    @GetMapping("/contactForm")
-    public String show(Model model){
-        model.addAttribute("ContactUs",new ContactUs());
-        return "index";
-    }
+//    @GetMapping("/contactForm")
+//    public String show(Model model){
+//        model.addAttribute("ContactUs",new ContactUs());
+//        return "index";
+//    }
 
     @PostMapping("/contactForm")
     public String submitConForm(@ModelAttribute ContactUs contactUs ,
                                 Model model){
         model.addAttribute("ContactUs",contactUs);
-        return "redirect:/message_send";
+        return "message_send";
     }
 }
