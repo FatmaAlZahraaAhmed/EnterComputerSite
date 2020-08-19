f3df4e2c885165f7acae6c77d17a349dbe53e32c
FatmaAlZahraaAhmed <48535599+FatmaAlZahraaAhmed@users.noreply.github.com>
2020-08-19 20:48:49 +0200

    style index

--- site/src/main/resources/static/assets/js/main.js
+++ site/src/main/resources/static/assets/js/main.js
@@ -126,6 +126,102 @@
     return false;
   });
 
+  /*Title: Cool Modal Popup Sign In/Out Form*/
+
+// $(function() {
+//   //defining all needed variables
+//   var $overlay = $('.overlay');
+//   var $mainPopUp = $('.main-popup')
+//   var $signIn = $('#sign-in');
+//   var $register = $('#register');
+//   var $formSignIn = $('form.sign-in');
+//   var $formRegister = $('form.register');
+  
+//   var $firstChild = $('nav ul li:first-child');
+//   var $secondChild = $('nav ul li:nth-child(2)');
+//   var $thirdChild = $('nav ul li:nth-child(3)');
+  
+//   //defining function to create underline initial state on document load
+//   function initialState() {
+//     $('.underline').css({
+//       "width": $firstChild.width(),
+//       "left": $firstChild.position().left,
+//       "top": $firstChild.position().top + $firstChild.outerHeight(true) + 'px'
+//     });
+//   }
+//   initialState(); //() used after calling function to call function immediately on doc load
+  
+//   //defining function to change underline depending on which li is active
+//   function changeUnderline(el) {
+//     $('.underline').css({
+//       "width": el.width(),
+//       "left": el.position().left,
+//       "top": el.position().top + el.outerHeight(true) + 'px'
+//     });
+//   } //note: have not called the function...don't want it called immediately
+  
+//   $firstChild.on('click', function(){
+//     var el = $firstChild;
+//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
+//     $secondChild.removeClass('active');
+//     $thirdChild.removeClass('active');
+//     $(this).addClass('active');
+//   });
+  
+//   $secondChild.on('click', function(){
+//     var el = $secondChild;
+//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
+//     $firstChild.removeClass('active');
+//     $thirdChild.removeClass('active');
+//     $(this).addClass('active');
+//   });
+  
+//   $thirdChild.on('click', function(){
+//     var el = $thirdChild;
+//     changeUnderline(el); //call the changeUnderline function with el as the perameter within the called function
+//     $firstChild.removeClass('active');
+//     $secondChild.removeClass('active');
+//     $(this).addClass('active');
+//   });
+  
+  
+//   $('button').on('click', function(){
+//     $overlay.addClass('visible');
+//     $mainPopUp.addClass('visible');
+//     $signIn.addClass('active');
+//     $register.removeClass('active');
+//     $formRegister.removeClass('move-left');
+//     $formSignIn.removeClass('move-left');
+//   });
+//   $overlay.on('click', function(){
+//     $(this).removeClass('visible');
+//     $mainPopUp.removeClass('visible');
+//   });
+//   $('#popup-close-button a').on('click', function(e){
+//     e.preventDefault();
+//     $overlay.removeClass('visible');
+//     $mainPopUp.removeClass('visible');
+//   });
+  
+//   $signIn.on('click', function(){
+//     $signIn.addClass('active');
+//     $register.removeClass('active');
+//     $formSignIn.removeClass('move-left');
+//     $formRegister.removeClass('move-left');
+//   });
+  
+//   $register.on('click', function(){
+//     $signIn.removeClass('active');
+//     $register.addClass('active');
+//     $formSignIn.addClass('move-left');
+//     $formRegister.addClass('move-left');
+//   });
+  
+//   $('input').on('submit', function(e){
+//     e.preventDefault(); //used to prevent submission of form...remove for real use
+//   });
+// });
+
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
@@ -144,28 +240,26 @@
     }
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
+ 
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
+// Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
   });
+
 
  
   // Init AOS
@@ -181,3 +275,4 @@
   });
 
 })(jQuery);
+
