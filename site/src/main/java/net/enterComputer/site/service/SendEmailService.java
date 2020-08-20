803fe016bbb06863e554b08b4efdf7340d241d72
FatmaAlZahraaAhmed <48535599+FatmaAlZahraaAhmed@users.noreply.github.com>
2020-08-20 21:32:27 +0200

    ready for sowt

--- site/src/main/java/net/enterComputer/site/service/SendEmailService.java
+++ site/src/main/java/net/enterComputer/site/service/SendEmailService.java
@@ -1,28 +1,9 @@
 package net.enterComputer.site.service;
 
+import net.enterComputer.site.model.SWOT;
 import net.enterComputer.site.model.Subscriber;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
 
-@Service
-public class SendEmailService {
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    public void sendEmail(Subscriber subscriber) {
-        System.out.println("message sending......");
-        SimpleMailMessage newMessage = new SimpleMailMessage();
-        newMessage.setTo(subscriber.getEmail());
-        newMessage.setFrom("totanoni25@gmail.com");
-        newMessage.setSubject("Welcome Subscriber");
-        newMessage.setText("Hi, and Greetings" +
-                "This is our site EnterComputer for Software Solution" +"\n"+
-                "created by Eng.Fatima Alzahraa," +"\n"+
-                "Eng.Shaimaa And Eng.Esraa." +"\n"+
-                "Thank you");
-        javaMailSender.send(newMessage);
-        System.out.println("send...ok");
-    }
+public interface SendEmailService {
+    void sendEmail(Subscriber subscriber);
+    void sendSWOT(SWOT swot);
 }
